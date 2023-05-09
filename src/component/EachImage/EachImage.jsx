f0dd8f30fd46fa404f0490fd4bf13aef16b8ef94
gorzerk1 <98368925+gorzerk1@users.noreply.github.com>
2023-05-09 05:14:34 +0300

    save

--- src/component/EachImage/EachImage.jsx
+++ src/component/EachImage/EachImage.jsx
@@ -6,39 +6,41 @@ import { MyContext } from '../../data/ThemeProvider';
 function EachImage() {
   const { imageIndex } = useContext(MyContext);
   const [index, setIndex] = useState(imageIndex || 0);
-  const [currentImage, setCurrentImage] = useState(GalleryData[0]?.src || '');
+  const [currentImage, setCurrentImage] = useState(GalleryData[0] || {});
+  const { src, width, height, isWide } = currentImage;
 
   useEffect(() => {
-    setCurrentImage(GalleryData[index]?.src || '');
+    setCurrentImage(GalleryData[index] || {});
   }, [index]);
 
-  function handleNext(){
+  function handleNext() {
     if (index < GalleryData.length - 1) {
       setIndex(index + 1);
     }
-  };
-  function handlePrev(){
+  }
+
+  function handlePrev() {
     if (index > 0) {
       setIndex(index - 1);
     }
-  };
-  
-  
+  }
+
+
   return (
     <div className="EachImage--body">
-      <img src={currentImage} alt="" />
-      <div className='EachImage--amountImages'>{`${index + 1} / ${GalleryData.length}`}</div>
+      <img src={src} alt="" />
+      <div className={`EachImage--amountImages ${isWide ? 'EachImage--isWide' : ""}`}>{`${index + 1} / ${GalleryData.length}`}</div>
       <div className='EachImage--container'>
-      <div className='EachImage--arrow'>
-        <img src="../../eventsup/left-arrow.png" onClick={handlePrev} alt="" />
-      </div>
-      <div className='EachImage--MainPicture'>
-        <img src={currentImage} alt="" />
-        <img src="../../download.png" alt="" className='download'/>
-      </div>
-      <div className='EachImage--arrow1'>
-        <img src="../../eventsup/right-arrow.png"  onClick={handleNext} alt="" />
-      </div>
+        <div className='EachImage--arrow'>
+          <img src="../../eventsup/left-arrow.png" onClick={handlePrev} alt="" />
+        </div>
+        <div className='EachImage--MainPicture EachImage--download'>
+          <img src={src} alt="" style={{ width, height }} />
+
+        </div>
+        <div className='EachImage--arrow1'>
+          <img src="../../eventsup/right-arrow.png" onClick={handleNext} alt="" />
+        </div>
       </div>
     </div>
   );
