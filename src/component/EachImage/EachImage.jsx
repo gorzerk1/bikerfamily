61e9fc22dd44b89a25f73ed51e063fba492aa08a
gorzerk1 <98368925+gorzerk1@users.noreply.github.com>
2023-05-12 03:24:30 +0300

    Update EachImage.jsx

--- src/component/EachImage/EachImage.jsx
+++ src/component/EachImage/EachImage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './eachImage.css';
 import { GalleryHeight, GalleryWidth } from '../../data/GalleryData.jsx';
 
