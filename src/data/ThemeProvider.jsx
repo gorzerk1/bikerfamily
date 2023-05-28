fcb81f9757838b1390a54fbcbdd3f07f69235b3c
gorzerk1 <98368925+gorzerk1@users.noreply.github.com>
2023-05-28 05:02:42 +0300

    Update ThemeProvider.jsx

--- src/data/ThemeProvider.jsx
+++ src/data/ThemeProvider.jsx
@@ -18,30 +18,40 @@ function ThemeProvider({ children }) {
         var s3 = new AWS.S3();
         var params = { Bucket: 'bikerpicture', Prefix: path };
         let tempImageList = [];
-
+    
         s3.listObjectsV2(params, function(err, data) {
           if (err) {
             console.log('Error listing S3 objects:', err, err.stack);
             reject(err);
           } else {
             var bucketContents = data.Contents;
-            bucketContents.forEach((bucketContent) => {
-              const urlParams = {Bucket: 'bikerpicture', Key: bucketContent.Key};
-
-              s3.getSignedUrl('getObject', urlParams, async (err, url) => {
-                if (err) {
-                  console.log('Error generating signed S3 URL:', err, err.stack);
-                  reject(err);
-                } else {
-                  tempImageList.push({ Key: bucketContent.Key, url: url });
-                  resolve(tempImageList);
-                }
+    
+            // Store promises for each getSignedUrl operation
+            let urlPromises = bucketContents.map(bucketContent => {
+              return new Promise((resolve, reject) => {
+                const urlParams = {Bucket: 'bikerpicture', Key: bucketContent.Key};
+    
+                s3.getSignedUrl('getObject', urlParams, async (err, url) => {
+                  if (err) {
+                    console.log('Error generating signed S3 URL:', err, err.stack);
+                    reject(err);
+                  } else {
+                    tempImageList.push({ Key: bucketContent.Key, url: url });
+                    resolve();
+                  }
+                });
               });
             });
+    
+            // After all promises are resolved, then resolve the main promise
+            Promise.all(urlPromises)
+              .then(() => resolve(tempImageList))
+              .catch((err) => reject(err));
           }
         });
       });
     };
+    
 
     const fetchData = async () => {
       const galleryWidthData = await fetchImagesFromAWS('width/');
