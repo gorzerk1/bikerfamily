437521cc8add47518451a403c407e4f947ba763f
gorzerk1 <98368925+gorzerk1@users.noreply.github.com>
2023-05-28 07:27:41 +0300

    Update ThemeProvider.jsx

--- src/data/ThemeProvider.jsx
+++ src/data/ThemeProvider.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import AWS from 'aws-sdk';
 
 const MyContext = React.createContext();
 
@@ -9,58 +8,29 @@ function ThemeProvider({ children }) {
   const [galleryWidth, setGalleryWidth] = useState([]);
 
   useEffect(() => {
-    AWS.config.update({
-      region: "eu-south-1"
-    });
-
-    const fetchImagesFromAWS = async (path) => {
-      return new Promise((resolve, reject) => {
-        var s3 = new AWS.S3();
-        var params = { Bucket: 'bikerpicture', Prefix: path };
-        let tempImageList = [];
-    
-        s3.listObjectsV2(params, function(err, data) {
-          if (err) {
-            console.log('Error listing S3 objects:', err, err.stack);
-            reject(err);
-          } else {
-            var bucketContents = data.Contents;
-    
-            // Store promises for each getSignedUrl operation
-            let urlPromises = bucketContents.map(bucketContent => {
-              return new Promise((resolve, reject) => {
-                const urlParams = {Bucket: 'bikerpicture', Key: bucketContent.Key};
-    
-                s3.getSignedUrl('getObject', urlParams, async (err, url) => {
-                  if (err) {
-                    console.log('Error generating signed S3 URL:', err, err.stack);
-                    reject(err);
-                  } else {
-                    tempImageList.push({ Key: bucketContent.Key, url: url });
-                    resolve();
-                  }
-                });
-              });
-            });
-    
-            // After all promises are resolved, then resolve the main promise
-            Promise.all(urlPromises)
-              .then(() => resolve(tempImageList))
-              .catch((err) => reject(err));
-          }
-        });
-      });
-    };
-    
-
-    const fetchData = async () => {
-      const galleryWidthData = await fetchImagesFromAWS('width/');
-      const galleryHeightData = await fetchImagesFromAWS('height/');
-      setGalleryWidth(galleryWidthData);
-      setGalleryHeight(galleryHeightData);
+    const fetchData = async (path) => {
+      // Send HTTP GET request to server endpoint
+      const response = await fetch(`http://backend-server-ip:port/api/images/${path}`);
+      if (!response.ok) {
+        throw new Error('HTTP error ' + response.status);
+      }
+      // Parse JSON response
+      const images = await response.json();
+      return images;
     };
 
-    fetchData();
+    const fetchGalleryData = async () => {
+      try {
+        const galleryWidthData = await fetchData('width');
+        const galleryHeightData = await fetchData('height');
+        setGalleryWidth(galleryWidthData);
+        setGalleryHeight(galleryHeightData);
+      } catch (err) {
+        console.error('Failed to fetch gallery data:', err);
+      }
+    };
+
+    fetchGalleryData();
   }, []);
 
   return (
