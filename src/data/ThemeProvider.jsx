c93a645405f790c76751e783efac74705a910555
gorzerk1 <98368925+gorzerk1@users.noreply.github.com>
2023-05-30 06:37:01 +0300

    update

--- src/data/ThemeProvider.jsx
+++ src/data/ThemeProvider.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { v4 as uuidv4 } from 'uuid'; // you'll need to install uuid
+import { v4 as uuidv4 } from 'uuid';
 
 const MyContext = React.createContext();
 
@@ -10,7 +10,7 @@ function ThemeProvider({ children }) {
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch(`http://35.152.45.97:3000/api/images`);
+      const response = await fetch(`http://18.102.116.122:3000/api/images`);
       if (!response.ok) {
         throw new Error('HTTP error ' + response.status);
       }
