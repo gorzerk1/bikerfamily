168b0fb2010659fb921346a34ac6dfb0b36477fa
gorzerk1 <98368925+gorzerk1@users.noreply.github.com>
2023-06-07 03:28:39 +0300

    Update Main.jsx

--- src/component/main/Main.jsx
+++ src/component/main/Main.jsx
@@ -1,57 +1,34 @@
 import './main.css';
-import { useState, useEffect, useRef, useContext } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { MyContext } from '../../data/ThemeProvider';
 
 function Main() {
   const { backGroundVideos } = useContext(MyContext);
-  
-  const [video, setVideo] = useState(() => {
-    if(backGroundVideos.length > 0) {
-      return backGroundVideos[Math.floor(Math.random() * backGroundVideos.length)];
-    }
-    return null;
-  });
-
-  const videoRef = useRef(null);
-
+  const [videoIndex, setVideoIndex] = useState(0);
+  const videoSource = window.innerWidth < 700 ? backGroundVideos[videoIndex]?.height : backGroundVideos[videoIndex]?.width;
+  console.log(backGroundVideos)
+  console.log("1")
   useEffect(() => {
-    const currentVideoRef = videoRef.current;
-
-    function handleResize() {
-      if (video && window.innerWidth < 700) {
-        currentVideoRef.src = video.height;
-      } else if(video) {
-        currentVideoRef.src = video.width;
-      }
+    const handleResize = () => {
+      setVideoIndex((videoIndex + 1) % backGroundVideos.length);
     };
 
-    function handleVideoEnd() {
-      setVideo(() => {
-        let newVideo;
-        do {
-          newVideo = backGroundVideos[Math.floor(Math.random() * backGroundVideos.length)];
-        } while(newVideo === video)
-        return newVideo;
-      });
-    }
+    const handleVideoEnd = () => {
+      setVideoIndex((videoIndex + 1) % backGroundVideos.length);
+    };
 
     window.addEventListener('resize', handleResize);
-    if (currentVideoRef) {
-      currentVideoRef.addEventListener('ended', handleVideoEnd);
-      handleResize();
-    }
+    window.addEventListener('ended', handleVideoEnd);
 
     return () => {
       window.removeEventListener('resize', handleResize);
-      if (currentVideoRef) {
-        currentVideoRef.removeEventListener('ended', handleVideoEnd);
-      }
+      window.removeEventListener('ended', handleVideoEnd);
     };
-  }, [video, backGroundVideos]);
+  }, [videoIndex, backGroundVideos]);
 
   return (
     <div className="Main--body">
-      <video ref={videoRef} autoPlay muted playsInline />
+      <video src={videoSource} autoPlay muted playsInline />
       <img src="../../down-arrow.png" alt="" className="Main--arrow" />
     </div>
   );
