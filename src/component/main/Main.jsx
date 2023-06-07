de0a0fab4b4548cea649632162773ec193041934
gorzerk1 <98368925+gorzerk1@users.noreply.github.com>
2023-06-07 03:58:25 +0300

    Update Main.jsx

--- src/component/main/Main.jsx
+++ src/component/main/Main.jsx
@@ -5,30 +5,31 @@ import { MyContext } from '../../data/ThemeProvider';
 function Main() {
   const { backGroundVideos } = useContext(MyContext);
   const [videoIndex, setVideoIndex] = useState(0);
+  const videoRef = useRef(null);
+
   const videoSource = window.innerWidth < 700 ? backGroundVideos[videoIndex]?.height : backGroundVideos[videoIndex]?.width;
-  console.log(backGroundVideos)
-  console.log("update (0001)")
+
   useEffect(() => {
-    const handleResize = () => {
-      setVideoIndex((videoIndex + 1) % backGroundVideos.length);
-    };
+    const currentVideoRef = videoRef.current;
 
     const handleVideoEnd = () => {
       setVideoIndex((videoIndex + 1) % backGroundVideos.length);
     };
 
-    window.addEventListener('resize', handleResize);
-    window.addEventListener('ended', handleVideoEnd);
+    if (currentVideoRef) {
+      currentVideoRef.addEventListener('ended', handleVideoEnd);
+    }
 
     return () => {
-      window.removeEventListener('resize', handleResize);
-      window.removeEventListener('ended', handleVideoEnd);
+      if (currentVideoRef) {
+        currentVideoRef.removeEventListener('ended', handleVideoEnd);
+      }
     };
   }, [videoIndex, backGroundVideos]);
 
   return (
     <div className="Main--body">
-      <video src={videoSource} autoPlay muted playsInline />
+      <video ref={videoRef} src={videoSource} autoPlay muted playsInline />
       <img src="../../down-arrow.png" alt="" className="Main--arrow" />
     </div>
   );
