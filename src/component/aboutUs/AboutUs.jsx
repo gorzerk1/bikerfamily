fec0a0f9b2e90a869561e66fa53a685e29963c9f
gorzerk1 <98368925+gorzerk1@users.noreply.github.com>
2023-06-12 13:20:10 +0300

    update

--- src/component/aboutUs/AboutUs.jsx
+++ src/component/aboutUs/AboutUs.jsx
@@ -21,15 +21,18 @@ function AboutUs() {
     <div className="aboutUs--body">
       <img src={backgroundImgSrc} alt="Background Bike" />
       <div className='aboutUs--container'>
-        <div className='aboutUs--half aboutUs--half--first'>
-          <div className='aboutUs--half__title'>עלינו</div>
+        <div className='aboutUs--rightPart'>
+          <div className='aboutUs--half__title'><div>עלינו</div></div>
           <div className='aboutUs--paragraph'>בקהילת האופנועים, קיימת אחדות מיוחדת בין חובבי הדרך הפתוחה והרוח החופשית שמספקות שתי הגלגלים. חברים מכל הגילאים, המגדרים והמוצאים מתאחדים כאחד בשביל להנות זה מזה ולחלוק את התרבות המשולבת של האופנועים. הרגשה של שייכות ותמיכה הדדית מלווה את חברי הקהילה, שמבוססת על כיף והרפתקאות משותפות.</div>
           <div className='aboutUs--paragraph'>דרך טיולים משולבים, אירועים חברתיים ופעילויות תרבותיות, קהילת האופנועים מחזיקה ביחד, כפועל רב-פרטי שקולע ללבבות של כל רוכב.</div>
-          <a className="custom-button" href='https://www.instagram.com/family.bikers.il/'>הצטרפו עלינו</a>
+          <div className='aboutUs--buttonWrapper'>
+          </div>          
+          
         </div>
-        <div className='aboutUs--half'>
-          <img src="../../bikerlogo.png" alt="Biker Logo" />
-          <div className='aboutUs--amounts'>
+        <div className='aboutUs--leftPart'>
+            <div className='aboutUs--logo'>
+              <img src="../../bikerlogo.png" alt="Biker Logo" />
+            </div>
             <div className='aboutUs--amount'>
               <div> חברי המשפחה</div>
               <div style={{color:"red"}}>300+</div>
@@ -37,8 +40,7 @@ function AboutUs() {
             <div className='aboutUs--amount'>
               <div>סה"כ אירועים</div>
               <div style={{color:"red"}}>50+</div>
-            </div>
-          </div>         
+            </div>     
         </div>
       </div>
     </div>
--- src/data/ThemeProvider.jsx
+++ src/data/ThemeProvider.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import { shuffle } from 'lodash'; // add this import at the beginning of your file
+import { shuffle } from 'lodash';
 
 const MyContext = React.createContext();
 
@@ -8,21 +8,27 @@ function ThemeProvider({ children }) {
   const [imageKey, setImageKey] = useState(null);
   const [galleryHeight, setGalleryHeight] = useState([]);
   const [galleryWidth, setGalleryWidth] = useState([]);
+  const [galleryHeightLow, setGalleryHeightLow] = useState([]);
+  const [galleryWidthLow, setGalleryWidthLow] = useState([]);
   const [backGroundVideos, setBackGroundVideo] = useState([]);
 
   const shuffleImages = (images) => {
     return shuffle(images).map(image => ({ ...image, key: uuidv4() }));
   }
-
+  console.log(galleryHeight)
+  console.log("asdsadsadsadsadsadasd")
+  console.log(galleryWidth)
   useEffect(() => {
     const fetchData = async () => {
       try {
+        const responseLow = await fetch(`https://api.bikersil.com/api/images/low`);
         const response = await fetch(`https://api.bikersil.com/api/images`);
-        if (!response.ok) {
+        if (!response.ok || !responseLow.ok) {
           throw new Error('HTTP error ' + response.status);
         }
 
         const images = await response.json();
+        const imagesLow = await responseLow.json();
 
         if (images.galleryWidth.length > 0) {
           const widthImages = shuffleImages(images.galleryWidth.slice(1));
@@ -32,6 +38,16 @@ function ThemeProvider({ children }) {
         if (images.galleryHeight.length > 0) {
           const heightImages = shuffleImages(images.galleryHeight.slice(1));
           setGalleryHeight(heightImages);
+        }
+
+        if (imagesLow.galleryWidthLow.length > 0) {
+          const widthImagesLow = shuffleImages(imagesLow.galleryWidthLow.slice(1));
+          setGalleryWidthLow(widthImagesLow);
+        }
+
+        if (imagesLow.galleryHeightLow.length > 0) {
+          const heightImagesLow = shuffleImages(imagesLow.galleryHeightLow.slice(1));
+          setGalleryHeightLow(heightImagesLow);
         }
       } catch (error) {
         console.error('Error:', error);
@@ -53,7 +69,6 @@ function ThemeProvider({ children }) {
   
         let videos = await response.json();
         
-        // If the videos array is not empty, remove the first element
         if (videos.length > 0) {
           videos.shift();
         }
@@ -74,6 +89,8 @@ function ThemeProvider({ children }) {
         imageKey,
         galleryHeight,
         galleryWidth,
+        galleryHeightLow,
+        galleryWidthLow,
         backGroundVideos
       }}
     >
