ef6a709f079d4da57f1293fdd4efbc6ecc554017
gorzerk1 <98368925+gorzerk1@users.noreply.github.com>
2023-06-13 23:52:43 +0300

    Fixes

--- src/component/gallery/Gallery.jsx
+++ src/component/gallery/Gallery.jsx
@@ -1,32 +1,13 @@
-import React, { useState, useContext, useEffect, useCallback } from 'react';
+import React, { useContext } from 'react';
 import './gallery.css';
 import { MyContext } from '../../data/ThemeProvider';
 import { Link } from "react-router-dom";
 
 function Gallery() {
   const { galleryHeight, galleryWidth, setImageKey } = useContext(MyContext);
-  const [heightImages, setHeightImages] = useState([]);
-  const [widthImages, setWidthImages] = useState([]);
 
-  const handleResize = useCallback(() => {
-    if (window.innerWidth <= 700) {
-      setHeightImages([...galleryHeight].slice(0, 2));
-      setWidthImages([...galleryWidth].slice(0, 4));
-    } else if (window.innerWidth <= 1200) {
-      setHeightImages([...galleryHeight].slice(0, 3));
-      setWidthImages([...galleryWidth].slice(0, 6));
-    } else {
-      setHeightImages([...galleryHeight].slice(0, 5));
-      setWidthImages([...galleryWidth].slice(0, 10));
-    }
-  }, [galleryHeight, galleryWidth]);
-  
 
-  useEffect(() => {
-    handleResize();
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, [handleResize]);
+
 
   return (
     <div className="gallery--body">
@@ -35,16 +16,21 @@ function Gallery() {
         <div>הטיולים שלנו</div>
       </div>
       <div className="gallery--container">
-      {widthImages.map((img, index) => (
+      {galleryWidth.map((img, index) => (
           <Link to="/fullGallery" className={`gallery--container--width${index + 1}`} onClick={() => setImageKey(img.key)}>
               <img src={img.src} alt="gallery" />
           </Link>
         ))}
-        {heightImages.map((img, index) => (
+        {galleryHeight.map((img, index) => (
           <Link to="/fullGallery" className={`gallery--container--height${index + 1}`} onClick={() => setImageKey(img.key)}>
               <img src={img.src} alt="gallery" />
           </Link>
         ))}
+      </div>
+      <div className='gallery--page'>
+        <img src="../../eventsup/left-arrow.png" alt="" />
+        <div>1 / {}</div>
+        <img src="../../eventsup/right-arrow.png" alt="" />        
       </div>
     </div>
   );
