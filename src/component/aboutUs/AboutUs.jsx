a431be392bcc4de2068c5c83bcba4bae5c2b426b
gorzerk1 <98368925+gorzerk1@users.noreply.github.com>
2023-06-15 05:11:15 +0300

    update

--- src/component/aboutUs/AboutUs.jsx
+++ src/component/aboutUs/AboutUs.jsx
@@ -1,37 +1,73 @@
+
 import './aboutUs.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { useSpring, animated } from 'react-spring';
+import VisibilitySensor from 'react-visibility-sensor';
 
 function AboutUs() {
 
+
+  const [titleVisible, setTitleVisible] = useState(false);
+  const [titleActive, setTitleActive] = useState(true);
+  const [firstParagraphVisible, setFirstParagraphVisible] = useState(false);
+  const [firstParagraphActive, setFirstParagraphActive] = useState(true);
+  const [secondParagraphVisible, setSecondParagraphVisible] = useState(false);
+  const [secondParagraphActive, setSecondParagraphActive] = useState(true);
+
+  const [logoVisible, setLogoVisible] = useState(false);
+  const [logoActive, setLogoActive] = useState(true);
+  const [firstAmountVisible, setFirstAmountVisible] = useState(false);
+  const [firstAmountActive, setFirstAmountActive] = useState(true);
+  const [secondAmountVisible, setSecondAmountVisible] = useState(false);
+  const [secondAmountActive, setSecondAmountActive] = useState(true);
+
+  const animationConfig = { duration: 1500 }; 
+
+  const titleAnimation = useSpring({ config: animationConfig, opacity: titleVisible ? 1 : 0, transform: titleVisible ? `translateY(0)` : `translateY(-50px)` });
+  const firstParagraphAnimation = useSpring({ config: animationConfig, opacity: firstParagraphVisible ? 1 : 0, transform: firstParagraphVisible ? `translateY(0)` : `translateY(-50px)` });
+  const secondParagraphAnimation = useSpring({ config: animationConfig, opacity: secondParagraphVisible ? 1 : 0, transform: secondParagraphVisible ? `translateY(0)` : `translateY(-50px)` });
+
+  const logoAnimation = useSpring({ config: animationConfig, opacity: logoVisible ? 1 : 0, transform: logoVisible ? `translateY(0)` : `translateY(50px)` });
+  const firstAmountAnimation = useSpring({ config: animationConfig, opacity: firstAmountVisible ? 1 : 0, transform: firstAmountVisible ? `translateY(0)` : `translateY(50px)` });
+  const secondAmountAnimation = useSpring({ config: animationConfig, opacity: secondAmountVisible ? 1 : 0, transform: secondAmountVisible ? `translateY(0)` : `translateY(50px)` });
 
   return (
     <div className="aboutUs--body">
       <img src="../../backgroundbike.jpg" alt="Background Bike" />
       <div className='aboutUs--container'>
         <div className='aboutUs--rightPart'>
-          <div className='aboutUs--half__title'><div>עלינו</div></div>
-          <div className='aboutUs--paragraph'>"בייקרס-ישראל" היא קהילה ייחודית של אופנוענים המונה יותר  מ300 חברים, והיא במגמת הרחבה וקולטת לשורותיה חברים נוספים, המתאימים לרוח הקהילה. ב"בייקרס-ישראל", חברים ישראלים בני כל הגילאים, ללא הבדל מגדר, דת או מוצא. לחבריי הקהילה אופנועים מסוגים ומודלים שונים, כשאת כולם מאחדת האהבה לאופנוע, החברות, והרוח החופשית שהדרך הפתוחה מעניקה ברכיבה הדו-גלגלית. הרגשה של שייכות ותמיכה הדדית מלווה את חבריי הקהילה, הרגשה המבוססת על חוויות והרפתקאות משותפות. את הקהילה הקימו ומנהלים יהונתן ודין. </div>
-          <div className='aboutUs--paragraph'>קהילת "בייקרס-ישראל", מארגנת לחבריה טיולים משולבים, אירועים חברתיים, ופעילויות חברתיות. הנהלת  "בייקרס-ישראל" קשובה לחבריי הקהילה באופן הקולע ללבו והעדפותיו של כל רוכב ורוכב.</div>
-          <div className='aboutUs--buttonWrapper'>
-          </div>          
-          
+          <VisibilitySensor active={titleActive} onChange={(isVisible) => { setTitleVisible(isVisible); setTitleActive(!isVisible); }} delayedCall partialVisibility>
+            <animated.div style={titleAnimation} className='aboutUs--half__title'><div>עלינו</div></animated.div>
+          </VisibilitySensor>
+          <VisibilitySensor active={firstParagraphActive} onChange={(isVisible) => { setFirstParagraphVisible(isVisible); setFirstParagraphActive(!isVisible); }} delayedCall partialVisibility>
+            <animated.div style={firstParagraphAnimation} className='aboutUs--paragraph'>"בייקרס-ישראל" היא קהילה ייחודית של אופנוענים המונה יותר  מ300 חברים, והיא במגמת הרחבה וקולטת לשורותיה חברים נוספים, המתאימים לרוח הקהילה. ב"בייקרס-ישראל", חברים ישראלים בני כל הגילאים, ללא הבדל מגדר, דת או מוצא. לחבריי הקהילה אופנועים מסוגים ומודלים שונים, כשאת כולם מאחדת האהבה לאופנוע, החברות, והרוח החופשית שהדרך הפתוחה מעניקה ברכיבה הדו-גלגלית. הרגשה של שייכות ותמיכה הדדית מלווה את חבריי הקהילה, הרגשה המבוססת על חוויות והרפתקאות משותפות. את הקהילה הקימו ומנהלים יהונתן ודין.</animated.div>
+          </VisibilitySensor>
+          <VisibilitySensor active={secondParagraphActive} onChange={(isVisible) => { setSecondParagraphVisible(isVisible); setSecondParagraphActive(!isVisible); }} delayedCall partialVisibility>
+            <animated.div style={secondParagraphAnimation} className='aboutUs--paragraph'>קהילת "בייקרס-ישראל", מארגנת לחבריה טיולים משולבים, אירועים חברתיים, ופעילויות חברתיות. הנהלת  "בייקרס-ישראל" קשובה לחבריי הקהילה באופן הקולע ללבו והעדפותיו של כל רוכב ורוכב.</animated.div>
+          </VisibilitySensor>
         </div>
         <div className='aboutUs--leftPart'>
-            <div className='aboutUs--logo'>
-              <img src="../../bikerlogo.png" alt="Biker Logo" />
-            </div>
-            <div className='aboutUs--amount'>
+          <VisibilitySensor active={logoActive} onChange={(isVisible) => { setLogoVisible(isVisible); setLogoActive(!isVisible); }} delayedCall partialVisibility>
+            <animated.div style={logoAnimation} className='aboutUs--logo'>
+              <img src="../../bikerlogo.png" alt="Biker Logo" />  
+            </animated.div>
+          </VisibilitySensor>
+          <VisibilitySensor active={firstAmountActive} onChange={(isVisible) => { setFirstAmountVisible(isVisible); setFirstAmountActive(!isVisible); }} delayedCall partialVisibility>
+            <animated.div style={firstAmountAnimation} className='aboutUs--amount'>
               <div> חברי המשפחה</div>
               <div style={{color:"red"}}>300+</div>
-            </div>
-            <div className='aboutUs--amount'>
-              <div>סה"כ אירועים</div>
-              <div style={{color:"red"}}>50+</div>
-            </div>     
+            </animated.div>
+          </VisibilitySensor>
+          <VisibilitySensor active={secondAmountActive} onChange={(isVisible) => { setSecondAmountVisible(isVisible); setSecondAmountActive(!isVisible); }} delayedCall partialVisibility>
+            <animated.div style={secondAmountAnimation} className='aboutUs--amount'>
+              <div>טיולים</div>
+              <div style={{color:"red"}}>30+</div>
+            </animated.div>
+          </VisibilitySensor>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
 export default AboutUs;
--- src/component/contact/Contact.jsx
+++ src/component/contact/Contact.jsx
@@ -1,19 +1,70 @@
 import React, { useState, useEffect } from "react";
+import { useSpring, animated } from 'react-spring';
 import "./contact.css";
 
 function Contact() {
   const [name, setName] = useState("");
   const [bike, setBike] = useState("");
   const [location, setLocation] = useState("");
-  const [instagram, setInstagram] = useState(""); 
+  const [instagram, setInstagram] = useState("");
   const [nameError, setNameError] = useState(false);
-  const [instagramError, setInstagramError] = useState(false); 
+  const [instagramError, setInstagramError] = useState(false);
   const [bikeError, setBikeError] = useState(false);
   const [locationError, setLocationError] = useState(false);
-  const [userHasClicked, setUserHasClicked] = useState({name: false, instagram: false, bike: false, location: false}); 
+  const [userHasClicked, setUserHasClicked] = useState({name: false, instagram: false, bike: false, location: false});
+
+  const titleProps = useSpring({
+    from: {opacity: 0, transform: 'translate3d(0,50px,0)'},
+    to: {opacity: 1, transform: 'translate3d(0,0px,0)'},
+    delay: 500,
+    config: {duration: 1000},
+  });
+
+  const subtitleProps = useSpring({
+    from: {opacity: 0, transform: 'translate3d(0,50px,0)'},
+    to: {opacity: 1, transform: 'translate3d(0,0px,0)'},
+    delay: 1000,
+    config: {duration: 1000},
+  });
+
+  const input1Props = useSpring({
+    from: {opacity: 0, transform: 'translate3d(0,50px,0)'},
+    to: {opacity: 1, transform: 'translate3d(0,0px,0)'},
+    delay: 1500,
+    config: {duration: 1000},
+  });
+
+  const input2Props = useSpring({
+    from: {opacity: 0, transform: 'translate3d(0,50px,0)'},
+    to: {opacity: 1, transform: 'translate3d(0,0px,0)'},
+    delay: 2000,
+    config: {duration: 1000},
+  });
+
+  const input3Props = useSpring({
+    from: {opacity: 0, transform: 'translate3d(0,50px,0)'},
+    to: {opacity: 1, transform: 'translate3d(0,0px,0)'},
+    delay: 2500,
+    config: {duration: 1000},
+  });
+
+  const input4Props = useSpring({
+    from: {opacity: 0, transform: 'translate3d(0,50px,0)'},
+    to: {opacity: 1, transform: 'translate3d(0,0px,0)'},
+    delay: 3000,
+    config: {duration: 1000},
+  });
+
+  const aProps = useSpring({
+    from: {opacity: 0, transform: 'translate3d(0,50px,0)'},
+    to: {opacity: 1, transform: 'translate3d(0,0px,0)'},
+    delay: 3500,
+    config: {duration: 1000},
+  });
 
   const whatsappMessage = `שלום שמי ${name}.\nאני גר/ה ב${location}.\nהאופנוע שיש לי הוא ${bike}.\nהאינסגרם שלי הוא :\n${instagram}.`;
   const whatsappLink = `https://chat.whatsapp.com/Bqfustz1sNaFuSa6ZRduJu?text=${encodeURIComponent(whatsappMessage)}`;
+
   function validate(){
     const nameRegex = /^[a-zA-Zא-ת\s]{2,}$/;
     let isValid = true;
@@ -81,26 +132,26 @@ function Contact() {
     <div className="contact--body">
       <img src="../../marblebackground2.jpg" alt="" />
       <div className='contact--container'>
-      <div className='contact--title'>הצטרפו אלינו</div>
-        <div className='contact--subtitle'>אנא מלאו את השדות שלפניכם</div>
+        <animated.div style={titleProps} className='contact--title'>הצטרפו אלינו</animated.div>
+        <animated.div style={subtitleProps} className='contact--subtitle'>אנא מלאו את השדות שלפניכם</animated.div>
         <div className='contact-textarea'>
-          <div>
+          <animated.div style={input1Props}>
             {nameError && <div className="contact--errors" dir="rtl">* צריך לרשום לפנות 4 ספרות בעברית או אנגלית</div>}
             <input type={nameError ? 'text1' : 'text'} placeholder="מה שמך ?" dir="rtl" value={name} onChange={(e) => {setName(e.target.value); setUserHasClicked(prev => ({...prev, name: true}))}} />
-          </div>
-          <div>
+          </animated.div>
+          <animated.div style={input2Props}>
             {bikeError && <div className="contact--errors" dir="rtl">* צריך לרשום את האופנוע שלך</div>}
             <input type={bikeError ? 'text1' : 'text'} placeholder="על מה את/ה רוכב/ת?" dir="rtl" value={bike} onChange={(e) => {setBike(e.target.value); setUserHasClicked(prev => ({...prev, bike: true}))}} />
-          </div>
-          <div>
+          </animated.div>
+          <animated.div style={input3Props}>
             {locationError && <div className="contact--errors" dir="rtl">* צריך לרשום מיקום</div>}
             <input type={locationError ? 'text1' : 'text'} placeholder="מאיפה בארץ ? (עיר \ ישוב)" dir="rtl" value={location} onChange={(e) => {setLocation(e.target.value); setUserHasClicked(prev => ({...prev, location: true}))}} />
-          </div>
-          <div>
-            {instagramError && <div className="contact--errors" dir="rtl">* צריך לרשום את הקישור של אינסטגרם</div>} 
+          </animated.div>
+          <animated.div style={input4Props}>
+            {instagramError && <div className="contact--errors" dir="rtl">* צריך לרשום את הקישור של אינסטגרם</div>}
             <input type={instagramError ? 'text1' : 'text'} placeholder="קישור של אינסטגרם" dir="rtl" value={instagram} onChange={(e) => {setInstagram(e.target.value); setUserHasClicked(prev => ({...prev, instagram: true}))}} />
-          </div>
-          <a href={whatsappLink} target="_blank" rel="noopener noreferrer" className="contact-whatsapp" onClick={(e) => { if (!validate()) e.preventDefault(); }}>צרו קשר דרך ווטסאפ</a>
+          </animated.div>
+          <animated.a style={aProps} href={whatsappLink} target="_blank" rel="noopener noreferrer" className="contact-whatsapp" onClick={(e) => { if (!validate()) e.preventDefault(); }}>צרו קשר דרך ווטסאפ</animated.a>
         </div>
       </div>
     </div>
