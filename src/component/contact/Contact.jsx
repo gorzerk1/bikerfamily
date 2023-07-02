344911cfcefef13255038d2760091daf365f620e
gorzerk1 <98368925+gorzerk1@users.noreply.github.com>
2023-07-02 14:27:01 +0300

    update

--- src/component/contact/Contact.jsx
+++ src/component/contact/Contact.jsx
@@ -17,11 +17,13 @@ function Contact() {
   const [bike, setBike] = useState("");
   const [location, setLocation] = useState("");
   const [instagram, setInstagram] = useState("");
+  const [telegram, setTelegram] = useState("");
   const [nameError, setNameError] = useState(false);
   const [instagramError, setInstagramError] = useState(false);
   const [bikeError, setBikeError] = useState(false);
   const [locationError, setLocationError] = useState(false);
-  const [userHasClicked, setUserHasClicked] = useState({name: false, instagram: false, bike: false, location: false});
+  const [telegramError, setTelegramError] = useState(false);
+  const [userHasClicked, setUserHasClicked] = useState({name: false, instagram: false, bike: false, location: false, telegram: false});
 
   const titleProps = useSpring({
     from: {opacity: 0, transform: 'translate3d(0,50px,0)'},
@@ -69,6 +71,13 @@ function Contact() {
     config: {duration: 1000},
     reset: !inView,
   });
+  const input5Props = useSpring({
+    from: {opacity: 0, transform: 'translate3d(0,50px,0)'},
+    to: {opacity: inView ? 1 : 0, transform: inView ? 'translate3d(0,0px,0)' : 'translate3d(0,50px,0)'},
+    delay: 3500,
+    config: {duration: 1000},
+    reset: !inView,
+  });
 
   const aProps = useSpring({
     from: {opacity: 0, transform: 'translate3d(0,50px,0)'},
@@ -81,67 +90,95 @@ function Contact() {
 
   function validate(){
     const nameRegex = /^[a-zA-Zא-ת\s]{2,}$/;
+    const telegramRegex = /^@[a-zA-Zא-ת]{3,}$/;
     let isValid = true;
-
+  
     if (!nameRegex.test(name)) {
       setNameError(true);
       isValid = false;
     } else {
       setNameError(false);
     }
-
+  
     if (!instagram.includes('instagram.com/')) {
       setInstagramError(true);
       isValid = false;
     } else {
       setInstagramError(false);
     }
-
+  
     if (!bike) {
       setBikeError(true);
       isValid = false;
     } else {
       setBikeError(false);
     }
-
+  
     if (!location) {
       setLocationError(true);
       isValid = false;
     } else {
       setLocationError(false);
     }
-
+  
+    if (!telegramRegex.test(telegram)) {
+      setTelegramError(true);
+      isValid = false;
+    } else {
+      setTelegramError(false);
+    }
+  
     return isValid;
   }
+  
 
   useEffect(() => {
     const nameRegex = /^[a-zA-Zא-ת\s]{2,}$/;
-
+    const telegramRegex = /^@[a-zA-Zא-ת]{3,}$/;
     if (!nameRegex.test(name) && userHasClicked.name) {
       setNameError(true);
     } else {
       setNameError(false);
     }
-
+  
     if (!instagram.includes('instagram.com/') && userHasClicked.instagram) {
       setInstagramError(true);
     } else {
       setInstagramError(false);
     }
-
+  
     if (!bike && userHasClicked.bike) {
       setBikeError(true);
     } else {
       setBikeError(false);
     }
-
+  
     if (!location && userHasClicked.location) {
       setLocationError(true);
     } else {
       setLocationError(false);
     }
-  }, [name, userHasClicked.name, instagram, userHasClicked.instagram, bike, userHasClicked.bike, location, userHasClicked.location]);
+  
 
+    if (!telegramRegex.test(telegram) && userHasClicked.telegram) {
+      setTelegramError(true);
+    } else {
+      setTelegramError(false);
+    }
+  }, [name, userHasClicked.name, instagram, userHasClicked.instagram, bike, userHasClicked.bike, location, userHasClicked.location, telegram, userHasClicked.telegram]);
+
+  function handleTelegramChange(e) {
+    const value = e.target.value;
+    setTelegram(value);
+    setUserHasClicked(prev => ({...prev, telegram: true}));
+  }
+  
+  function handleTelegramFocus(e) {
+    if (e.target.value === '') {
+      setTelegram('@');
+    }
+  }
+  
   return (
     <div className="contact--body" ref={ref}>
       <img src="../../marblebackground2.jpg" alt="" />
@@ -162,16 +199,27 @@ function Contact() {
             <input type={locationError ? 'text1' : 'text'} placeholder="מאיפה בארץ ? (עיר \ ישוב)" dir="rtl" value={location} onChange={(e) => {setLocation(e.target.value); setUserHasClicked(prev => ({...prev, location: true}))}} />
           </animated.div>
           <animated.div style={input4Props}>
+              {telegramError && <div className="contact--errors" dir="rtl">צריך לרשום @ בהתחלה *</div>}
+              <input 
+                type={telegramError ? 'text1' : 'text'} 
+                placeholder="שם משתמש בטלגרם"  
+                dir="rtl" 
+                value={telegram} 
+                onFocus={handleTelegramFocus}
+                onChange={handleTelegramChange}
+              />
+            </animated.div>
+          <animated.div style={input5Props}>
             {instagramError && <div className="contact--errors" dir="rtl">* צריך לרשום את הקישור של אינסטגרם</div>}
             <input type={instagramError ? 'text1' : 'text'} placeholder="קישור של אינסטגרם" dir="rtl" value={instagram} onChange={(e) => {setInstagram(e.target.value); setUserHasClicked(prev => ({...prev, instagram: true}))}} />
           </animated.div>
           <animated.a style={aProps} target="_blank" rel="noopener noreferrer" className="contact-whatsapp" onClick={(e) => { 
               if (validate()) {
-                context.addContact({name, bike, location, instagram});
+                context.addContact({name, bike, location, instagram, telegram});
               } else {
                 e.preventDefault();
               }
-            }}>צרו קשר דרך ווטסאפ</animated.a>
+            }}>צרו קשר דרך טלגרם</animated.a>
         </div>
       </div>
     </div>
