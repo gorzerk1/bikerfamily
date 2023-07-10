6dbbbb588dc92dd9c79c117a8386ef2838b693df
gorzerk1 <98368925+gorzerk1@users.noreply.github.com>
2023-07-10 08:55:14 +0300

    Update Contact.jsx

--- src/component/contact/Contact.jsx
+++ src/component/contact/Contact.jsx
@@ -181,7 +181,7 @@
     }, [name, userHasClicked.name, instagram, userHasClicked.instagram, bike, userHasClicked.bike, location, userHasClicked.location, telegram, userHasClicked.telegram]);
 
     async function checkIfUserExists(telegram) {
-      const response = await fetch(`https://api.bikersil.com/api/contact/exist?telegram=${telegram}`);
+      const response = await fetch(`https://bikersil.com/api/contact/exist?telegram=${telegram}`);
       if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
       }
